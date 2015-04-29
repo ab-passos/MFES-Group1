c46178291256567d594ebcac56ef9cb4097850db
Carlos Brito <cfabrito@gmail.com>
2015-04-29 11:42:32 +0100

    Clean main

--- assignment1/main.c
+++ assignment1/main.c
@@ -7,34 +7,10 @@
 
 extern sensor_t *sensor_result_queue;
 
-
-
 void print_sensor(sensor_t *sensor);
 
 int main()
 {
-	sensor_t sensor;
-
-	init_sensor(&sensor);
-
-	scan_result_t result;
-
-	result.id = 1;
-	result.request_type = REQUEST_X;
-	result.result_x = (SENSOR_A_get_result_x_t *) malloc (sizeof(SENSOR_A_get_result_x_t));
-
-	result.result_x->value1 = 0.0f;
-	result.result_x->value2 = 0.1f;
-
-	add_result(&sensor, &result);
-
-	scan_result_t *da_result = NULL;
-	get_result(&sensor, 2, &da_result, REQUEST_X);
-
-	printf("Did it work? %d\n", da_result != NULL);
-
-	print_sensor(&sensor);
-
 	return 0;
 }
 
